f6442088b9bd7f53db948a5285110753221a99c2
Matt Jones <matthewj866@gmail.com>
2022-04-10 21:04:21 +0100

    Fix for file persistence.

--- Assets/Scripts/ACHNarrativeDriver/Editor/NarrativeSequenceEditor.cs
+++ Assets/Scripts/ACHNarrativeDriver/Editor/NarrativeSequenceEditor.cs
@@ -22,22 +22,31 @@ namespace ACHNarrativeDriver.Editor
             }
             
             GUILayout.Label("Source Script", EditorStyles.label);
+            var previousSourceScript = _currentNarrativeSequence.SourceScript;
             _currentNarrativeSequence.SourceScript = GUILayout.TextArea(_currentNarrativeSequence.SourceScript,
                 GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
 
+            var previousSequence = _currentNarrativeSequence.NextSequence;
             _currentNarrativeSequence.NextSequence = (NarrativeSequence)EditorGUILayout.ObjectField("Next Narrative Sequence", _currentNarrativeSequence.NextSequence, typeof(NarrativeSequence), false);
-            
-            if (GUILayout.Button("Save Source Script", GUILayout.MinWidth(500)))
+
+            bool compiledScriptChanged = false;
+            if (GUILayout.Button("Save Source Script"))
             {
+                compiledScriptChanged = true;
                 var listOfStuff = _interpreter.Interpret(_currentNarrativeSequence.SourceScript);
                 _currentNarrativeSequence.CharacterDialoguePairs = listOfStuff;
+            }
+
+            if (_currentNarrativeSequence.SourceScript != previousSourceScript || _currentNarrativeSequence.NextSequence != previousSequence || compiledScriptChanged)
+            {
+                EditorUtility.SetDirty(_currentNarrativeSequence);
             }
         }
 
         [MenuItem("Window / ACH Narrative Driver / Narrative Sequence Editor")]
         public static void ShowEditor()
         {
-            var window = EditorWindow.GetWindow<NarrativeSequenceEditor>();
+            var window = EditorWindow.GetWindow<NarrativeSequenceEditor>(title: "Narrative Sequence Editor");
             window.minSize = new Vector2(500, 500);
         }
     }
