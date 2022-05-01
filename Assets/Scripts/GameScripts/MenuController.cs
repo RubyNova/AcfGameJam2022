11e16efcbcb3536690e86c962e3e38ed0dc39229
Rosalina <abdulazizareda@gmail.com>
2022-05-01 11:08:22 -0400

    User name entering and minigame audio

--- Assets/Scripts/GameScripts/MenuController.cs
+++ Assets/Scripts/GameScripts/MenuController.cs
@@ -11,45 +11,58 @@ public class MenuController : MonoBehaviour
 {
     [SerializeField] private GameObject mainMenuObject;
     [SerializeField] private GameObject optionsMenuObject;
+    [SerializeField] private GameObject namingMenuObject;
+    [SerializeField] private ACHNarrativeDriver.Api.RuntimeVariables narrativeRuntimeVariableObject;
     [SerializeField] private TextMeshProUGUI volumeDisplay;
     [SerializeField] private Image currentBG;
     [SerializeField] private Sprite mainMenuBG;
-    [SerializeField] private Sprite optionsMenuBG;
+    [SerializeField] private Sprite sideMenuBG;
     [SerializeField] private Slider volumeSlider;
     [SerializeField] private AudioController audioController; // I'd normally use underscore prefix but this is not my file
+    [SerializeField] private TMP_InputField inputField;
     
     void Start()
     {
-        currentBG.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        inputField.text = "";
+        currentBG.color = new Color(0.9150943f, 0.6681915f, 0.6707927f, 1.0f);
         currentBG.sprite = mainMenuBG;
         optionsMenuObject.SetActive(false);
+        namingMenuObject.SetActive(false);
         mainMenuObject.SetActive(true);
         volumeSlider.value = audioController.Volume;
         OnVolumeSliderValueChanged(); // This is needed because UNITY UBER S U C C C C C C C
     }
-    
-    void Update()
+
+    public void UpdateNameAndStartGame()
     {
+        narrativeRuntimeVariableObject.UpdateVariable("player", inputField.text);
+        SceneManager.LoadScene("GameScene");
     }
 
     public void StartGame()
     {
-        SceneManager.LoadScene("GameScene");
+        currentBG.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        currentBG.sprite = sideMenuBG;
+        optionsMenuObject.SetActive(false);
+        namingMenuObject.SetActive(true);
+        mainMenuObject.SetActive(false);
     }
 
     public void OptionsMenu()
     {
         currentBG.color = new Color(0.2399788f, 0.1243681f, 0.4811321f, 1.0f);
-        currentBG.sprite = optionsMenuBG;
+        currentBG.sprite = sideMenuBG;
         optionsMenuObject.SetActive(true);
+        namingMenuObject.SetActive(false);
         mainMenuObject.SetActive(false);
     }
 
     public void BackToMainMenu()
     {
-        currentBG.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        currentBG.color = new Color(0.9150943f, 0.6681915f, 0.6707927f, 1.0f);
         currentBG.sprite = mainMenuBG;
         optionsMenuObject.SetActive(false);
+        namingMenuObject.SetActive(false);
         mainMenuObject.SetActive(true);
     }
 
